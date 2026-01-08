d77b5bf1b0351745489e06dc6e63dfa36bbfd9d5
Alper Kaya <93725692+alperkaya007@users.noreply.github.com>
2026-01-08 12:00:00 +0000

    Add transaction categories

---
+++ lib/utils/categories.dart
@@ -0,0 +1,6 @@
+const List<String> expenseCategories = [
+  "Food", "Transport", "Shopping", "Bills", "Health", "Entertainment", "Other"
+];
+const List<String> incomeCategories = [
+  "Salary", "Freelance", "Investment", "Other"
+];
